9ccdf682c0146330732c3003272b689a204c4636
Nathan Hu <nahu725@gmail.com>
2023-02-27 15:30:51 -0500

    updated the main for socket addring

--- book_store/src/main.rs
+++ book_store/src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use xml_rpc::server::*;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, ToSocketAddrs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let server_name = match args.get(1) {
+        Some(a) => a.to_string() + &".cs.williams.edu:8013".to_string(),
+        None => panic!("Enter a server name!"),
+    };
+
+    let ip_addr: SocketAddr = match server_name.to_socket_addrs().expect("Bad Server Name").next() {
+        Some(a) => a,
+        None => panic!("Server did not match an ip addr!"),
+    };
+
+    let server: Server = Server::new();
+    let response = server.bind(&ip_addr);
 }
